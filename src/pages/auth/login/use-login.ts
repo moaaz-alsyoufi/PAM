074ad36f90cbed9996bc15275eb07a6e0598ca29
Moaaz <moaaz.syf@gmail.com>
2024-10-18 20:48:21 +0300

    activate login with backend

--- src/pages/auth/login/use-login.ts
+++ src/pages/auth/login/use-login.ts
@@ -1,68 +1,79 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
-import { useNavigate, useSearchParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { z } from "zod";
-
 import useToast from "@/hooks/use-toast";
-import httpRequest from "@/services/api/request";
 import routes from "@/services/routes";
 import { useAuthContext } from "@/states/auth";
+import { ACTIVE_API_URL } from "@/services/api/api";
+import axios from "axios";
 
 const useLogin = () => {
-    const navigate = useNavigate();
-    const [searchParams] = useSearchParams();
-    const { toaster } = useToast();
+  const navigate = useNavigate();
 
-    const { setLoggedInUser } = useAuthContext();
+  const { toaster } = useToast();
+  const { setLoggedInUser } = useAuthContext();
 
-    const [isLoading, setIsLoading] = useState(false);
-    const [showPassword, setShowPassword] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
 
-    const toggleShowPassword = () => {
-        setShowPassword(!showPassword);
+  const toggleShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const loginSchema = z.object({
+    username: z.string(),
+    password: z.string(),
+  });
+
+  type LoginSchemaType = z.infer<typeof loginSchema>;
+
+  const { control, handleSubmit } = useForm<LoginSchemaType>({
+    resolver: zodResolver(loginSchema),
+  });
+
+  const onSubmit = handleSubmit(async (data) => {
+    setIsLoading(true);
+
+    const loginDate = {
+      email: data.username,
+      password: data.password,
     };
 
-    const loginSchema = z.object({
-        email: z.string().email(),
-        password: z.string(),
-    });
+    try {
+      const response = await axios.post(`${ACTIVE_API_URL}login`, loginDate);
 
-    type LoginSchemaType = z.infer<typeof loginSchema>;
+      toaster.success("Login successful...");
+      setLoggedInUser(response.data.token);
 
-    const { control, handleSubmit, setError } = useForm<LoginSchemaType>({
-        resolver: zodResolver(loginSchema),
-        defaultValues: {
-            email: "admin@daisyui.com",
-            password: "password",
-        },
-    });
+      console.log(response.data.token);
 
-    const transformErrorToForm = (errors: Record<string, any>) => {
-        Object.entries(errors).forEach(([key, value]: any[]) => setError(key, { message: value }));
-    };
+      setIsLoading(false);
+      navigate(routes.dashboards.ecommerce);
+    } catch (error: any) {
+      console.log("error: ", error);
 
-    const onSubmit = handleSubmit(async (data) => {
-        setIsLoading(true);
-        try {
-            const response = await httpRequest.post("/api/auth/login/", data);
-            setLoggedInUser(response.data);
-            toaster.success("Login successfully...");
-            const redirectTo = searchParams.get("redirectTo");
-            navigate(redirectTo ?? routes.home);
-        } catch (e: any) {
-            transformErrorToForm(e.response.data);
-        }
+      if (error.response) {
+        console.log("error.response: ", error.response);
+        toaster.error(`${error.response.data}`);
         setIsLoading(false);
-    });
+        throw new Error(error.response.data);
+      } else {
+        toaster.error("An error occurred. Please try again.");
+        setIsLoading(false);
+        throw new Error("An error occurred while logging in.");
+      }
+    }
+  });
 
-    return {
-        showPassword,
-        isLoading,
-        control,
-        onSubmit,
-        toggleShowPassword,
-    };
+  return {
+    showPassword,
+    isLoading,
+    control,
+    onSubmit,
+    toggleShowPassword,
+  };
 };
 
 export default useLogin;
