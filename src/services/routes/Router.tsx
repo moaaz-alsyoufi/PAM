50a8eafdcb0c95993a1732d4b12651f4a3a57099
Moaaz <moaaz.syf@gmail.com>
2024-10-18 21:31:31 +0300

    set the default route path to login page

--- src/services/routes/Router.tsx
+++ src/services/routes/Router.tsx
@@ -1,5 +1,5 @@
 import { Suspense } from "react";
-import { Navigate, Route, RouteProps, Routes, useLocation } from "react-router-dom";
+import { Navigate, Route, Routes, useLocation } from "react-router-dom";
 
 import AdminLayout from "@/components/layout/admin";
 import AuthLayout from "@/components/layout/auth";
@@ -7,43 +7,58 @@ import routes from "@/services/routes";
 import allRoutes from "@/services/routes/all_routes";
 import { useAuthContext } from "@/states/auth";
 
-const Router = (props: RouteProps) => {
-    const { isLoggedIn } = useAuthContext();
-    const location = useLocation();
+const Router = () => {
+  const { isLoggedIn } = useAuthContext();
+  const location = useLocation();
 
-    return (
-        <Routes>
-            <Route>
-                {allRoutes.admin.map((route, index) => (
-                    <Route
-                        key={"admin-" + index}
-                        path={route.path}
-                        element={
-                            isLoggedIn() ? (
-                                <AdminLayout {...props}>{route.element}</AdminLayout>
-                            ) : (
-                                <Navigate to={routes.auth.login + `?redirectTo=${location.pathname}`} replace />
-                            )
-                        }
-                    />
-                ))}
-            </Route>
-            <Route>
-                {allRoutes.auth.map((route, index) => (
-                    <Route
-                        key={"auth-" + index}
-                        path={route.path}
-                        element={<AuthLayout {...props}>{route.element}</AuthLayout>}
-                    />
-                ))}
-            </Route>
-            <Route>
-                {allRoutes.other.map((route, index) => (
-                    <Route key={"other-" + index} path={route.path} element={<Suspense>{route.element}</Suspense>} />
-                ))}
-            </Route>
-        </Routes>
-    );
+  return (
+    <Routes>
+      {/* Redirect root "/" to the login page */}
+      <Route path="/" element={<Navigate to={routes.auth.login} replace />} />
+
+      {/* Admin routes */}
+      <Route>
+        {allRoutes.admin.map((route, index) => (
+          <Route
+            key={"admin-" + index}
+            path={route.path}
+            element={
+              isLoggedIn() ? (
+                <AdminLayout>{route.element}</AdminLayout>
+              ) : (
+                <Navigate
+                  to={routes.auth.login + `?redirectTo=${location.pathname}`}
+                  replace
+                />
+              )
+            }
+          />
+        ))}
+      </Route>
+
+      {/* Auth routes */}
+      <Route>
+        {allRoutes.auth.map((route, index) => (
+          <Route
+            key={"auth-" + index}
+            path={route.path}
+            element={<AuthLayout>{route.element}</AuthLayout>}
+          />
+        ))}
+      </Route>
+
+      {/* Other routes */}
+      <Route>
+        {allRoutes.other.map((route, index) => (
+          <Route
+            key={"other-" + index}
+            path={route.path}
+            element={<Suspense>{route.element}</Suspense>}
+          />
+        ))}
+      </Route>
+    </Routes>
+  );
 };
 
 export default Router;
