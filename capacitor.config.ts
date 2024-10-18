a23de884d43d91d8ed4dfb8ca57f979a11366e43
melejel2 <eng.mejel@hotmail.com>
2024-10-18 22:59:06 +0300

    added capacitor ios

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.karamentreprises.pam',
+  appName: 'PAM PORTAL',
+  webDir: 'dist'
+};
+
+export default config;
