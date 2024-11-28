acd97a5922ed9ce24498afcb8df47b531d2ff452
Moaaz <moaaz.syf@gmail.com>
2024-11-28 15:12:13 +0200

    separate dashboard and admin tools pages

--- src/data/layout/admin/menu.ts
+++ src/data/layout/admin/menu.ts
@@ -12,81 +12,13 @@ import MapIcon from "@iconify/icons-lucide/map-pinned";
 import routes from "@/services/routes";
 import { IMenuItem } from "@/types/layout/admin";
 
-export const adminMenuItems: IMenuItem[] = [
+export const dashboardMenuItems: IMenuItem[] = [
   {
     key: "dashboard",
     icon: airplayIcon,
     label: "Dashboard",
     url: routes.dashboard.index,
   },
-  {
-    key: "admin_tools",
-    label: "Admin Tools",
-    icon: adminIcon,
-    notAdmin: true,
-    children: [
-      {
-        key: "companies",
-        icon: packageIcon,
-        label: "Companies",
-        // url: routes.dashboard.companies.index,
-        notAdmin: true,
-      },
-      {
-        key: "administrators",
-        icon: usersIcon,
-        label: "Administrators",
-        // url: routes.dashboard.administrators.index,
-        notAdmin: true,
-      },
-      {
-        key: "cost-code-library",
-        icon: DeliveryEntityIcon,
-        label: "Cost Code Library",
-        // url: routes.dashboard.costCodeLibrary.index,
-        notAdmin: true,
-      },
-      {
-        key: "categories-and-items",
-        icon: DeliveryEntityIcon,
-        label: "Categories and Items",
-        // url: routes.dashboard.categoriesAndItems.index,
-        notAdmin: true,
-      },
-      {
-        key: "branch-data",
-        icon: DeliveryEntityIcon,
-        label: "Branch Data",
-        children: [
-          {
-            key: "sites",
-            icon: packageCheckIcon,
-            label: "Sites",
-            // url: routes.dashboard.sites.index,
-          },
-          {
-            key: "users",
-            icon: packageCheckIcon,
-            label: "Users",
-            // url: routes.dashboard.users.index,
-          },
-          {
-            key: "suppliers",
-            icon: packageCheckIcon,
-            label: "Suppliers",
-            // url: routes.dashboard.suppliers.index,
-          },
-          {
-            key: "subcontractors",
-            icon: packageCheckIcon,
-            label: "Subcontractors",
-            // url: routes.dashboard.subcontractors.index,
-          },
-        ],
-      },
-    ],
-  },
-
   {
     key: "operations",
     label: "Operations",
@@ -96,31 +28,31 @@ export const adminMenuItems: IMenuItem[] = [
         key: "requests",
         icon: packageCheckIcon,
         label: "Requests",
-        // url: routes.dashboard.requests.index,
+        url: routes.dashboard.requests.index,
       },
       {
         key: "purchase-orders",
         icon: packageCheckIcon,
         label: "Purchase Orders",
-        // url: routes.dashboard.purchaseOrders.index,
+        url: routes.dashboard.purchaseOrders.index,
       },
       {
         key: "delivery-notes",
         icon: packageCheckIcon,
         label: "Delivery Notes",
-        // url: routes.dashboard.deliveryNotes.index,
+        url: routes.dashboard.deliveryNotes.index,
       },
       {
         key: "payment-orders",
         icon: PaymentsIcon,
         label: "Payment Orders",
-        // url: routes.dashboard.paymentOrders.index,
+        url: routes.dashboard.paymentOrders.index,
       },
       {
         key: "return-slips",
         icon: packageCheckIcon,
         label: "Return Slips",
-        // url: routes.dashboard.returnSlips.index,
+        url: routes.dashboard.returnSlips.index,
       },
     ],
   },
@@ -133,25 +65,25 @@ export const adminMenuItems: IMenuItem[] = [
         key: "site-movement",
         icon: packageCheckIcon,
         label: "Site Movement",
-        // url: routes.dashboard.siteMovement.index,
+        url: routes.dashboard.siteMovement.index,
       },
       {
         key: "procurement-statistics",
         icon: packageCheckIcon,
         label: "Procurement Statistics",
-        // url: routes.dashboard.procurementStatistics.index,
+        url: routes.dashboard.procurementStatistics.index,
       },
       {
         key: "payment-statistics",
         icon: packageCheckIcon,
         label: "Payment Statistics",
-        // url: routes.dashboard.paymentStatistics.index,
+        url: routes.dashboard.paymentStatistics.index,
       },
       {
         key: "site-stock",
         icon: packageCheckIcon,
         label: "Site Stock",
-        // url: routes.dashboard.siteStock.index,
+        url: routes.dashboard.siteStock.index,
       },
     ],
   },
@@ -159,12 +91,77 @@ export const adminMenuItems: IMenuItem[] = [
     key: "ai",
     icon: MapIcon,
     label: "AI",
-    // url: routes.dashboard.ai.index,
+    url: routes.dashboard.ai.index,
   },
   {
     key: "tutorials",
     icon: chatIcon,
     label: "tutorials",
-    // url: routes.dashboard.tutorials.index,
+    url: routes.dashboard.tutorials.index,
+  },
+];
+
+export const adminToolsMenuItems: IMenuItem[] = [
+  {
+    key: "admin_tools",
+    label: "Admin Tools",
+    icon: adminIcon,
+    children: [
+      {
+        key: "companies",
+        icon: packageIcon,
+        label: "Companies",
+        url: routes.adminTools.companies.index,
+      },
+      {
+        key: "administrators",
+        icon: usersIcon,
+        label: "Administrators",
+        url: routes.adminTools.administrators.index,
+      },
+      {
+        key: "cost-code-library",
+        icon: DeliveryEntityIcon,
+        label: "Cost Code Library",
+        url: routes.adminTools.costCodeLibrary.index,
+      },
+      {
+        key: "categories-and-items",
+        icon: DeliveryEntityIcon,
+        label: "Categories and Items",
+        url: routes.adminTools.categoriesAndItems.index,
+      },
+    ],
+  },
+  {
+    key: "branch-data",
+    icon: DeliveryEntityIcon,
+    label: "Branch Data",
+    children: [
+      {
+        key: "sites",
+        icon: packageCheckIcon,
+        label: "Sites",
+        url: routes.adminTools.branchData.sites.index,
+      },
+      {
+        key: "users",
+        icon: packageCheckIcon,
+        label: "Users",
+        url: routes.adminTools.branchData.users.index,
+      },
+      {
+        key: "suppliers",
+        icon: packageCheckIcon,
+        label: "Suppliers",
+        url: routes.adminTools.branchData.suppliers.index,
+      },
+      {
+        key: "subcontractors",
+        icon: packageCheckIcon,
+        label: "Subcontractors",
+        url: routes.adminTools.branchData.subcontractors.index,
+      },
+    ],
   },
 ];
