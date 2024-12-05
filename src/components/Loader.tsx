cbd5d7f161237e6f36bd643c6d4f43d27db9b1be
Moaaz <moaaz.syf@gmail.com>
2024-12-05 11:55:02 +0200

    create general loader component

---
+++ src/components/Loader.tsx
@@ -0,0 +1,7 @@
+export const Loader = () => {
+  return (
+    <div className="w-full h-[80vh] flex justify-center items-center">
+      <span className="loading loading-ring w-48 text-info"></span>
+    </div>
+  );
+};
