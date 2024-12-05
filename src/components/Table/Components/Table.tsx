b337bcbbe9fc0bd02dccb5a4a166a5b0e2394c49
Moaaz <moaaz.syf@gmail.com>
2024-12-05 12:25:01 +0200

    complete table component logic

--- src/components/Table/Components/Table.tsx
+++ src/components/Table/Components/Table.tsx
@@ -1,3 +1,5 @@
+import chevronLeftIcon from "@iconify/icons-lucide/chevron-left";
+import chevronRightIcon from "@iconify/icons-lucide/chevron-right";
 import pencilIcon from "@iconify/icons-lucide/pencil";
 import plusIcon from "@iconify/icons-lucide/plus";
 import searchIcon from "@iconify/icons-lucide/search";
@@ -10,105 +12,62 @@ import {
   Card,
   CardBody,
   Input,
-  Table,
-  TableBody,
-  TableHead,
-  TableRow,
+  Pagination,
   useDialog,
 } from "@/components/daisyui";
 import Icon from "@/components/Icon";
 import { IUser } from "@/types/apps/ecommerce";
 import DialogComponent from "./Dialog";
+import { cn } from "@/helpers/utils/cn";
 
-interface RowProps {
-  user: IUser;
-  onEdit: (user: IUser) => void;
-  onDelete: (id: number) => void;
+interface TableProps {
+  tableData: any[];
+  columns: string[];
+  actions: boolean;
 }
 
-const Row: React.FC<RowProps> = ({ user, onEdit, onDelete }) => {
-  const handleDelete = () => {
-    onDelete; // (id)
-  };
-
-  return (
-    <TableRow className="cursor-pointer hover:bg-base-200/40">
-      <></>
-
-      <div className="inline-flex w-fit">
-        <Button
-          color="ghost"
-          size="sm"
-          shape={"square"}
-          aria-label="Edit user"
-          onClick={() => onEdit(user)}
-        >
-          <Icon
-            icon={pencilIcon}
-            className="text-base-content/70"
-            fontSize={15}
-          />
-        </Button>
-        <Button
-          color="ghost"
-          className="text-error/70 hover:bg-error/20"
-          size="sm"
-          shape={"square"}
-          aria-label="Delete user"
-          onClick={handleDelete}
-        >
-          <Icon icon={trashIcon} className="" fontSize={16} />
-        </Button>
-      </div>
-    </TableRow>
-  );
-};
-
-const TableComponent = () => {
-  const [users, setUsers] = useState<IUser[]>([]);
+const TableComponent: React.FC<TableProps> = ({
+  tableData,
+  columns,
+  actions,
+}) => {
   const [sortColumn, setSortColumn] = useState<string | null>(null);
   const [sortOrder, setSortOrder] = useState<"asc" | "desc">("asc");
   const [searchQuery, setSearchQuery] = useState<string>("");
   const [dialogType, setDialogType] = useState<"Add" | "Edit">("Add");
-  const [currentUser, setCurrentUser] = useState<IUser | null>(null);
+  const [currentRow, setCurrentRow] = useState<IUser | null>(null);
   const { dialogRef, handleShow, handleHide } = useDialog();
 
   const filteredData = useMemo(() => {
     const lowercasedQuery = searchQuery.toLowerCase();
-    return users.filter((user) => {
-      return Object.values(user).some(
+    return tableData.filter((d) =>
+      Object.values(d).some(
         (value) =>
           typeof value === "string" &&
           value.toLowerCase().includes(lowercasedQuery)
-      );
-    });
-  }, [searchQuery, users]);
+      )
+    );
+  }, [searchQuery, tableData]);
 
   const sortedData = useMemo(() => {
-    const sortedData = [...filteredData];
+    if (!sortColumn) return filteredData;
 
-    if (sortColumn) {
-      sortedData.sort((a, b) => {
-        const aValue = a[sortColumn as keyof IUser];
-        const bValue = b[sortColumn as keyof IUser];
+    return [...filteredData].sort((a, b) => {
+      const aValue = a[sortColumn as keyof IUser];
+      const bValue = b[sortColumn as keyof IUser];
 
-        if (!aValue || !bValue) return 0;
-        if (aValue < bValue) return sortOrder === "asc" ? -1 : 1;
-        if (aValue > bValue) return sortOrder === "asc" ? 1 : -1;
-        return 0;
-      });
-    }
-
-    return sortedData;
+      if (!aValue || !bValue) return 0;
+      if (aValue < bValue) return sortOrder === "asc" ? -1 : 1;
+      if (aValue > bValue) return sortOrder === "asc" ? 1 : -1;
+      return 0;
+    });
   }, [filteredData, sortColumn, sortOrder]);
 
   const handleSort = (column: string) => {
-    if (sortColumn === column) {
-      setSortOrder(sortOrder === "asc" ? "desc" : "asc");
-    } else {
-      setSortColumn(column);
-      setSortOrder("asc");
-    }
+    setSortOrder((prevOrder) =>
+      sortColumn === column && prevOrder === "asc" ? "desc" : "asc"
+    );
+    setSortColumn(column);
   };
 
   const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -117,31 +76,36 @@ const TableComponent = () => {
 
   const openDialog = () => {
     setDialogType("Add");
-    setCurrentUser(null);
+    setCurrentRow(null);
     handleShow();
   };
 
   const openEditDialog = (user: IUser) => {
     setDialogType("Edit");
-    setCurrentUser(user);
+    setCurrentRow(user);
     handleShow();
   };
 
-  const handleDeleteUser = (id: number) => {
-    setUsers((prevUsers) => prevUsers.filter((user) => user.id !== id));
+  const handleDelete = (id: number) => {
+    console.log(`Delete row with ID: ${id}`);
+  };
+
+  const handleSuccess = () => {
+    console.log("Dialog action completed!");
+    handleHide();
   };
 
   return (
     <>
       <Card className="bg-base-100">
-        <CardBody className={"p-0"}>
+        <CardBody className="p-0">
           <div className="flex flex-col sm:flex-row items-start sm:items-center justify-start sm:justify-between px-5 pt-5 space-y-4 sm:space-y-0">
             <Button
               onClick={openDialog}
               className="btn btn-ghost btn-xs h-8 border border-base-content/20"
             >
               <Icon icon={plusIcon} fontSize={16} />
-              Add User
+              Add
             </Button>
             <div className="form-control flex flex-row items-center rounded-box border border-base-content/20 px-2">
               <Icon
@@ -151,7 +115,7 @@ const TableComponent = () => {
               />
               <Input
                 size="sm"
-                placeholder="Search users"
+                placeholder="Search data"
                 bordered={false}
                 borderOffset={false}
                 className="w-full focus:border-transparent focus:outline-0"
@@ -161,121 +125,113 @@ const TableComponent = () => {
             </div>
           </div>
           <div className="overflow-auto">
-            <Table className="mt-2 rounded-box">
-              <TableHead>
-                <span
-                  className="text-sm font-medium text-base-content/80 cursor-pointer flex space-x-2 items-center"
-                  onClick={() => handleSort("id")}
-                >
-                  <span>ID</span>
-                  {sortColumn === "id" && (
-                    <Icon
-                      icon={sortOrder === "asc" ? sortAscIcon : sortDescIcon}
-                      className="ml-1"
-                      fontSize={14}
-                    />
+            <table className="w-full border-collapse">
+              <thead>
+                <tr className="hover:bg-base-200/40">
+                  {columns.map((column, index) => (
+                    <th
+                      key={column}
+                      className={cn(
+                        "border-b border-base-content/5 px-2 pl-6 py-3 text-sm text-left font-normal",
+                        {
+                          "pl-6": index === 0,
+                        }
+                      )}
+                    >
+                      {column}
+                    </th>
+                  ))}
+                  {actions && (
+                    <th className="border-b border-base-content/5 pl-2 pr-6 py-3 text-sm text-right  font-normal">
+                      Actions
+                    </th>
                   )}
-                </span>
-                <span
-                  className="text-sm font-medium text-base-content/80 cursor-pointer flex space-x-2 items-center"
-                  onClick={() => handleSort("name")}
-                >
-                  <span>Name</span>
-                  {sortColumn === "name" && (
-                    <Icon
-                      icon={sortOrder === "asc" ? sortAscIcon : sortDescIcon}
-                      className="ml-1"
-                      fontSize={14}
-                    />
-                  )}
-                </span>
-                <span
-                  className="text-sm font-medium text-base-content/80 cursor-pointer flex space-x-2 items-center"
-                  onClick={() => handleSort("username")}
-                >
-                  <span>Username</span>
-                  {sortColumn === "username" && (
-                    <Icon
-                      icon={sortOrder === "asc" ? sortAscIcon : sortDescIcon}
-                      className="ml-1"
-                      fontSize={14}
-                    />
-                  )}
-                </span>
-                <span
-                  className="text-sm font-medium text-base-content/80 cursor-pointer flex space-x-2 items-center"
-                  onClick={() => handleSort("phone")}
-                >
-                  <span>Phone</span>
-                  {sortColumn === "phone" && (
-                    <Icon
-                      icon={sortOrder === "asc" ? sortAscIcon : sortDescIcon}
-                      className="ml-1"
-                      fontSize={14}
-                    />
-                  )}
-                </span>
-                <span
-                  className="text-sm font-medium text-base-content/80 cursor-pointer flex space-x-2 items-center"
-                  onClick={() => handleSort("deliveryCost")}
-                >
-                  <span>Delivery Cost</span>
-                  {sortColumn === "deliveryCost" && (
-                    <Icon
-                      icon={sortOrder === "asc" ? sortAscIcon : sortDescIcon}
-                      className="ml-1"
-                      fontSize={14}
-                    />
-                  )}
-                </span>
-                <span
-                  className="text-sm font-medium text-base-content/80 cursor-pointer flex space-x-2 items-center"
-                  onClick={() => handleSort("loyaltyPoints")}
-                >
-                  <span>Loyalty Points</span>
-                  {sortColumn === "loyaltyPoints" && (
-                    <Icon
-                      icon={sortOrder === "asc" ? sortAscIcon : sortDescIcon}
-                      className="ml-1"
-                      fontSize={14}
-                    />
-                  )}
-                </span>
-                <span className="text-sm font-medium text-base-content/80">
-                  Verified
-                </span>
-                <span className="text-sm font-medium text-base-content/80">
-                  Role
-                </span>
-                <span
-                  className="text-sm font-medium text-base-content/80 cursor-pointer flex space-x-2 items-center"
-                  onClick={() => handleSort("DeliveryEntity")}
-                >
-                  <span>Delivery Entity</span>
-                  {sortColumn === "DeliveryEntity" && (
-                    <Icon
-                      icon={sortOrder === "asc" ? sortAscIcon : sortDescIcon}
-                      className="ml-1"
-                      fontSize={14}
-                    />
-                  )}
-                </span>
+                </tr>
+              </thead>
+              <tbody>
+                {tableData.length > 0 ? (
+                  tableData.map((row, index) => (
+                    <tr key={index} className="hover:bg-base-200/40">
+                      {columns.map((column) => (
+                        <td
+                          key={column}
+                          className="border-y border-base-content/5 px-2 pl-6 py-3 font-medium text-sm"
+                        >
+                          {row[column] ?? "-"}
+                        </td>
+                      ))}
+                      {actions && (
+                        <td className="border-y border-base-content/5 px-2 py-3 font-medium text-sm text-right pr-6">
+                          <div className="inline-flex w-fit">
+                            <Button
+                              color="ghost"
+                              size="sm"
+                              shape="square"
+                              aria-label="Edit Row"
+                              onClick={() => openEditDialog(row)}
+                            >
+                              <Icon
+                                icon={pencilIcon}
+                                className="text-base-content/70"
+                                fontSize={15}
+                              />
+                            </Button>
 
-                <span className="text-sm font-medium text-base-content/80 ml-2.5">
-                  Actions
-                </span>
-              </TableHead>
-              <TableBody>
-                {sortedData.map((user) => (
-                  <Row
-                    key={user.id}
-                    user={user}
-                    onEdit={openEditDialog}
-                    onDelete={handleDeleteUser} // Pass the handleDeleteUser function
-                  />
-                ))}
-              </TableBody>
-            </Table>
+                            <Button
+                              color="ghost"
+                              className="text-error/70 hover:bg-error/20"
+                              size="sm"
+                              shape="square"
+                              aria-label="Delete Row"
+                              onClick={() => handleDelete(row.id)}
+                            >
+                              <Icon icon={trashIcon} fontSize={16} />
+                            </Button>
+                          </div>
+                        </td>
+                      )}
+                    </tr>
+                  ))
+                ) : (
+                  <tr className="cursor-pointer hover:bg-base-200/40">
+                    <td
+                      colSpan={columns.length + (actions ? 1 : 0)}
+                      className="p-2 text-center"
+                    >
+                      No data available
+                    </td>
+                  </tr>
+                )}
+              </tbody>
+            </table>
+          </div>
+          <div className="flex items-center justify-end px-5 pb-5 pt-3">
+            <Pagination>
+              <Button
+                size="sm"
+                aria-label="pagination-prev"
+                className="join-item"
+                startIcon={<Icon icon={chevronLeftIcon} fontSize={16} />}
+              />
+              <Button
+                size="sm"
+                className="join-item"
+                active
+                color="primary"
+                aria-label="pagination-1"
+              >
+                1
+              </Button>
+              <Button size="sm" className="join-item" aria-label="pagination-2">
+                2
+              </Button>
+              <Button
+                size="sm"
+                aria-label="pagination-next"
+                className="join-item"
+                startIcon={<Icon icon={chevronRightIcon} fontSize={16} />}
+              />
+            </Pagination>
           </div>
         </CardBody>
       </Card>
@@ -283,8 +239,8 @@ const TableComponent = () => {
         dialogRef={dialogRef}
         handleHide={handleHide}
         dialogType={dialogType}
-        currentUser={currentUser}
-        onSuccess={() => {}}
+        current={currentRow}
+        onSuccess={handleSuccess}
       />
     </>
   );
