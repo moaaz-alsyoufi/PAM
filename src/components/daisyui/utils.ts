b49d70536df1fce93d1ad1b4f6c3ab223043988f
melejel2 <eng.mejel@hotmail.com>
2024-12-07 18:58:07 +0200

    try fix some errors (not sure)

--- src/components/daisyui/utils.ts
+++ src/components/daisyui/utils.ts
@@ -1,62 +1,56 @@
-import { Children, Fragment, ReactElement, ReactNode, cloneElement, isValidElement } from "react";
-import { twMerge } from "tailwind-merge";
+import { cloneElement, Fragment, isValidElement, ReactElement, ReactNode } from 'react';
+import { twMerge } from 'tailwind-merge';
 
-export const toTitleCase = (str: string) => {
-    return str
-        .toLowerCase()
-        .split(" ")
-        .map(function (word) {
-            return word.charAt(0).toUpperCase() + word.slice(1);
-        })
-        .join(" ");
-};
+interface BaseProps {
+  className?: string;
+  children?: ReactNode;
+  [key: string]: any;
+}
 
-// Returns true if an element is a react fragment
-export const isReactFragment = (node: ReactNode | typeof Fragment) => {
-    if (!node) return false;
+interface WrapperProps extends BaseProps {
+  onClick?: () => void;
+}
 
-    if ((node as ReactElement)?.type) {
-        return (node as ReactElement)?.type === Fragment;
-    }
+interface ReactElementWithProps extends ReactElement {
+  props: BaseProps;
+}
 
-    return node === Fragment;
-};
-
-// If an invalid element or fragment is passed in as the node, wrap it with the wrapper and add props
-// If a valid element is passed, add the props
-export const wrapWithElementIfInvalid = ({
-    node,
-    wrapper,
-    props = {},
+export const wrapElement = ({
+  node,
+  wrapper,
+  props = {},
 }: {
-    node: ReactNode;
-    wrapper: ReactElement;
-    props?: any;
+  node: ReactNode;
+  wrapper: ReactElement;
+  props?: WrapperProps;
 }) => {
-    if (!node) {
-        return cloneElement(wrapper, props);
-    } else if (!isValidElement(node)) {
-        return cloneElement(wrapper, props, node);
-    } else if (isReactFragment(node)) {
-        return cloneElement(
-            wrapper,
-            { ...props, className: twMerge(node.props?.className, props?.className) },
-            node.props.children,
-        );
-    } else {
-        return cloneElement(node, {
-            ...props,
-            className: twMerge(node.props?.className, props?.className),
-        });
-    }
+  if (!node) {
+    return cloneElement<BaseProps>(wrapper, props);
+  }
+
+  if (!isValidElement(node)) {
+    return cloneElement<BaseProps>(wrapper, props, node);
+  }
+
+  const nodeElement = node as ReactElementWithProps;
+
+  if (isReactFragment(nodeElement)) {
+    return cloneElement<BaseProps>(
+      wrapper,
+      {
+        ...props,
+        className: twMerge(nodeElement.props?.className, props?.className),
+      },
+      nodeElement.props.children,
+    );
+  }
+
+  return cloneElement<BaseProps>(nodeElement, {
+    ...props,
+    className: twMerge(nodeElement.props?.className, props?.className),
+  });
 };
 
-// Returns true if there is a single, string child element
-export const isSingleStringChild = (children?: ReactNode) => {
-    return (
-        children &&
-        Children.count(children) === 1 &&
-        isValidElement(children) &&
-        typeof children.props.children === "string"
-    );
+export const isReactFragment = (node: ReactElement): boolean => {
+  return node.type === Fragment || node.type?.toString() === 'Symbol(react.fragment)';
 };
