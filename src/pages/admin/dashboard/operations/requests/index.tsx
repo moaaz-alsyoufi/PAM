70f08217a0df25720fdf4fab2f77c8d0d10e4ab5
Moaaz <moaaz.syf@gmail.com>
2024-12-12 16:31:14 +0200

    get and pass requests data to its table

--- src/pages/admin/dashboard/operations/requests/index.tsx
+++ src/pages/admin/dashboard/operations/requests/index.tsx
@@ -1,15 +1,22 @@
 import PageMetaData from "@/components/PageMetaData";
 import PageTitle from "@/components/PageTitle";
 import PAMTable from "@/components/Table";
+import useRequests from "./use-requests";
 
 const Requests = () => {
+  const { columns, tableData, inputFields } = useRequests();
   return (
     <div>
       <PageMetaData title={"Requests"} />
 
       <PageTitle title={"Requests"} subMenu={"Dashboard"} center="Operations" />
       <div>
-        <PAMTable />
+        <PAMTable
+          columns={columns}
+          tableData={tableData}
+          inputFields={inputFields}
+          actions={true}
+        />
       </div>
     </div>
   );
