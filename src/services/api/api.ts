d1190554d96c45b310b384ccc8f812f1904330e3
Moaaz <moaaz.syf@gmail.com>
2024-12-22 15:51:22 +0200

    define the general api url

--- src/services/api/api.ts
+++ src/services/api/api.ts
@@ -1,2 +1,4 @@
-export const ACTIVE_API_URL =
-  "https://pamapis.karamentreprises.com/api/Login/";
+// export const ACTIVE_URL = "https://localhost:7050/";
+export const ACTIVE_URL = "https://pamapis.karamentreprises.com/";
+
+export const ACTIVE_API_URL = `${ACTIVE_URL}api/`;
