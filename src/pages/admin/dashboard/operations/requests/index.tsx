6da144c743ecedd840ad2b895954213606e038f9
Moaaz <moaaz.syf@gmail.com>
2024-12-25 00:12:24 +0200

    activate loading on get data in requests page

--- src/pages/admin/dashboard/operations/requests/index.tsx
+++ src/pages/admin/dashboard/operations/requests/index.tsx
@@ -3,12 +3,13 @@ import PageTitle from "@/components/PageTitle";
 import PAMTable from "@/components/Table";
 import useRequests from "./use-requests";
 import { useAuthContext } from "@/states/auth";
+import { Loader } from "@/components/Loader";
 
 const Requests = () => {
   const { authState } = useAuthContext();
   const siteId = authState.user?.siteid || 0;
   const token = authState.user?.token || "";
-  const { columns, tableData, inputFields, hasActions } = useRequests(
+  const { columns, tableData, inputFields, hasActions, loading } = useRequests(
     siteId,
     token
   );
@@ -17,21 +18,26 @@ const Requests = () => {
       <PageMetaData title={"Requests"} />
 
       <PageTitle title={"Requests"} subMenu={"Dashboard"} center="Operations" />
-      <div>
-        {siteId === 0 ? (
-          <p>Please select a valid site to view requests.</p>
-        ) : tableData.length > 0 ? (
-          <PAMTable
-            columns={columns}
-            tableData={tableData}
-            inputFields={inputFields}
-            actions={hasActions}
-            title="Request"
-          />
-        ) : (
-          <p>No data available</p> // Ensure this message is conditional
-        )}
-      </div>
+      {loading ? (
+        <Loader />
+      ) : (
+        <div>
+          {siteId === 0 ? (
+            <p>Please select a valid site to view requests.</p>
+          ) : tableData.length > 0 ? (
+            <PAMTable
+              columns={columns}
+              tableData={tableData}
+              inputFields={inputFields}
+              actions={hasActions}
+              title="Request"
+              loading={loading}
+            />
+          ) : (
+            <p>No data available</p> // Ensure this message is conditional
+          )}
+        </div>
+      )}
     </div>
   );
 };
