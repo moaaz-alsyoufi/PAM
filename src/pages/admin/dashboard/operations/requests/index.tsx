d1f2a3976b470a52f802244c4768d6568a192348
Moaaz <moaaz.syf@gmail.com>
2025-01-04 10:19:13 +0200

    open static dialog from request page instead the dynamic dialog

--- src/pages/admin/dashboard/operations/requests/index.tsx
+++ src/pages/admin/dashboard/operations/requests/index.tsx
@@ -4,9 +4,13 @@ import PAMTable from "@/components/Table";
 import useRequests from "./use-requests";
 import { useAuthContext } from "@/states/auth";
 import { Loader } from "@/components/Loader";
+import DialogComponent from "@/components/Table/Components/Dialog";
+import { useDialog } from "@/components/daisyui";
 
 const Requests = () => {
   const { authState } = useAuthContext();
+  const { dialogRef, handleShow, handleHide } = useDialog();
+
   const siteId = authState.user?.siteid || 0;
   const {
     columns,
@@ -20,7 +24,17 @@ const Requests = () => {
   const roleId = authState.user.roleid;
 
   const canMakeNewRequest =
-    roleId === 4 || roleId === 5 || roleId === 7 || roleId === 10;
+    roleId === 1 ||
+    roleId === 4 ||
+    roleId === 5 ||
+    roleId === 7 ||
+    roleId === 10;
+
+  const handleOpenDialog = (type: string, data: any) => {
+    console.log(type);
+    console.log(data);
+    handleShow();
+  };
 
   return (
     <div>
@@ -34,17 +48,31 @@ const Requests = () => {
           {siteId === 0 ? (
             <p>Please select a valid site to view requests.</p>
           ) : tableData.length > 0 ? (
-            <PAMTable
-              columns={columns}
-              tableData={tableData}
-              inputFields={inputFields}
-              title="Request"
-              loading={loading}
-              actions={hasActions}
-              showAction={true}
-              previewColumns={previewColumns}
-              addBtn={canMakeNewRequest}
-            />
+            <>
+              <PAMTable
+                columns={columns}
+                tableData={tableData}
+                inputFields={inputFields}
+                title="Request"
+                loading={loading}
+                actions={hasActions}
+                showAction={true}
+                previewColumns={previewColumns}
+                addBtn={canMakeNewRequest}
+                dynamicDialog={false}
+                openStaticDialog={handleOpenDialog}
+              />
+
+              <DialogComponent
+                handleHide={handleHide}
+                dialogRef={dialogRef}
+                dialogType={"Add"}
+                current={{}}
+                onSuccess={() => {}} // Pass success handler
+                inputFields={inputFields}
+                title=""
+              />
+            </>
           ) : (
             <p>No data available</p> // Ensure this message is conditional
           )}
