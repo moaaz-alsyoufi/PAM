9c09a2f6661b8ff50e0a8b4086a486fa8d86d08a
Moaaz <moaaz.syf@gmail.com>
2025-01-04 10:14:03 +0200

    create request dialog instead of use dynamic dialog

---
+++ src/pages/admin/dashboard/operations/requests/components/RequestDialog.tsx
@@ -0,0 +1,248 @@
+import React, { useState, useEffect } from "react";
+import { Button, Select, SelectOption } from "@/components/daisyui";
+import useToast from "@/hooks/use-toast";
+import { useAuthContext } from "@/states/auth";
+import PAMTable from "..";
+import { cn } from "@/helpers/utils/cn";
+import useRequests from "../use-requests";
+
+interface InputField {
+  name: string;
+  type: string;
+  value?: any;
+  required?: boolean;
+  options?: any[];
+}
+
+interface CurrentData {
+  [key: string]: any;
+}
+
+interface DialogProps {
+  handleHide: () => void;
+  dialogRef: React.RefObject<HTMLDialogElement | null>;
+  dialogType: "Add" | "Edit" | "Preview";
+  current: CurrentData | null;
+  onSuccess: (type: "Add" | "Edit" | "Preview", formData: any) => void;
+  inputFields?: InputField[];
+  previewColumns?: Record<string, string>;
+  title: string;
+  data?: any[];
+}
+
+const RequestDialog: React.FC<DialogProps> = ({
+  handleHide,
+  dialogRef,
+  dialogType,
+  current,
+  onSuccess,
+  inputFields,
+  title,
+  previewColumns,
+  data,
+}) => {
+  // Initialize form data based on inputFields and current data
+  const [formData, setFormData] = useState<Record<string, any>>(() => {
+    const initialData: Record<string, any> = {};
+    inputFields?.forEach((field) => {
+      if (
+        dialogType === "Edit" &&
+        current &&
+        current[field.name] !== undefined
+      ) {
+        initialData[field.name] = current[field.name];
+      } else {
+        initialData[field.name] = field.value || "";
+      }
+    });
+    return initialData;
+  });
+
+  const [isLoading, setIsLoading] = useState(false);
+  const { toaster } = useToast();
+  const { getToken } = useAuthContext();
+  const { exportRequest } = useRequests();
+
+  // Optional: Update formData when current changes (e.g., when editing a different user)
+  useEffect(() => {
+    if (dialogType === "Edit" && current) {
+      setFormData((prevData) => ({
+        ...prevData,
+        ...current,
+      }));
+    }
+  }, [current, dialogType]);
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setIsLoading(true);
+
+    console.log(formData);
+
+    try {
+      const token = getToken();
+      if (!token) {
+        toaster.error("Token is missing, unable to save.");
+        return;
+      }
+
+      if (dialogType === "Edit" && current) {
+        // Example: API call to update user
+        // await updateUserApi(formData, token);
+      } else if (dialogType === "Add") {
+        // Example: API call to add user
+        // await addUserApi(formData, token);
+      } else if (dialogType === "Preview") {
+        try {
+          const pdfBlob = await exportRequest(current?.materialId);
+          const pdfUrl = URL.createObjectURL(pdfBlob);
+          window.open(pdfUrl, "_blank");
+          console.log("PDF opened successfully");
+        } catch (error) {
+          console.error("Error opening PDF:", error);
+        }
+      }
+
+      toaster.success(
+        `${dialogType === "Edit" ? "updated" : "created"} successfully.`
+      );
+      onSuccess(dialogType, formData);
+    } catch (error: any) {
+      console.error("Error saving user:", error);
+      if (error.response) {
+        toaster.error(
+          `Failed to save user. Server responded with status ${error.response.status}: ${error.response.data}`
+        );
+      } else if (error.request) {
+        toaster.error(
+          "Failed to save user. No response received from the server."
+        );
+      } else {
+        toaster.error(`Failed to save user. Error: ${error.message}`);
+      }
+    } finally {
+      setIsLoading(false);
+      handleHide();
+    }
+  };
+
+  const handleClose = () => {
+    handleHide();
+  };
+
+  // Dynamically render inputs based on inputFields
+  const renderInput = (field: InputField) => {
+    const { name, type, required, options } = field;
+    {
+      if (type === "select") {
+        return (
+          <label className="input input-sm input-bordered flex items-center xs:gap-4 lg:gap-12 text-sm md:text-base">
+            <span className="font-normal opacity-45 w-20 capitalize">
+              {name}
+            </span>
+            <Select
+              className="w-full border-none focus:outline-none focus:ring-0 bg-transparent"
+              onChange={(e) =>
+                setFormData({ ...formData, [name]: e.target.value })
+              }
+              name={name}
+              value={formData[name]}
+              required={required}
+              onTouchStart={(e) => {
+                if (e.touches.length > 1) {
+                  e.preventDefault();
+                }
+              }}
+            >
+              {(options ?? []).map((option) => (
+                <SelectOption
+                  key={option}
+                  value={option}
+                  className="bg-base-100"
+                >
+                  {option.name}
+                </SelectOption>
+              ))}
+            </Select>
+          </label>
+        );
+      } else {
+        return (
+          <label
+            className="flex flex-col sm:flex-row items-center gap-2 input input-sm input-bordered"
+            key={name}
+          >
+            <span className="font-normal text-sm md:text-base opacity-45 min-w-16 md:w-28">
+              {name.charAt(0).toUpperCase() + name.slice(1)}
+            </span>
+            <input
+              type={type}
+              name={name}
+              className="grow"
+              value={formData[name]}
+              required={required}
+              onChange={(e) =>
+                setFormData({ ...formData, [name]: e.target.value })
+              }
+            />
+          </label>
+        );
+      }
+    }
+  };
+
+  return (
+    <dialog ref={dialogRef} className="modal" aria-modal="true">
+      <div
+        className={cn("modal-box relative", {
+          "max-w-7xl": dialogType === "Preview",
+        })}
+      >
+        <button
+          type="button"
+          className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
+          onClick={handleClose}
+          aria-label="Close"
+        >
+          ✕
+        </button>
+        <h3 className="font-bold text-lg">{title}</h3>
+
+        <form onSubmit={handleSubmit}>
+          {dialogType === "Preview" ? (
+            <PAMTable
+              columns={previewColumns ?? {}}
+              tableData={data ?? []}
+              inputFields={[]}
+              actions={false}
+              title="Request Details"
+            />
+          ) : (
+            <div className="space-y-3 my-4">
+              {inputFields?.map((field) => (
+                <div key={field.name}>{renderInput(field)}</div>
+              ))}
+            </div>
+          )}
+
+          <div className="text-right mt-5">
+            <Button
+              className="w-full btn btn-sm"
+              type="submit"
+              disabled={isLoading}
+              loading={isLoading}
+            >
+              {dialogType === "Add"
+                ? "Add"
+                : dialogType === "Edit"
+                  ? "Save"
+                  : "Export"}
+            </Button>
+          </div>
+        </form>
+      </div>
+    </dialog>
+  );
+};
+
+export default RequestDialog;
