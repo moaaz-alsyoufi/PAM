bc9a3bd76679eec2a9d5c68edfa1b812bd258398
CapitalAcademy <saadismail001@gmail.com>
2025-01-13 18:25:51 +0400

    Update Routes with user role

--- src/services/routes/Router.tsx
+++ src/services/routes/Router.tsx
@@ -14,8 +14,35 @@ import allRoutes from "@/services/routes/all_routes";
 import { useAuthContext } from "@/states/auth";
 
 const Router = (props: RouteProps) => {
-  const { isLoggedIn } = useAuthContext();
+  // const useAuthContext = () => {
+  //   const role = 4; // Replace with actual logic to fetch the role
+  //   const isLoggedIn = () => true; // Replace with actual auth check
+  //   return { isLoggedIn, getRole: () => role };
+  // };
+  const { isLoggedIn, restrictedRoles } = useAuthContext();
   const location = useLocation();
+
+  // Allowed roles for limited admin tools
+
+  // Filter Admin Tools Routes based on the role
+  const getFilteredAdminRoutes = () => {
+    if (restrictedRoles) {
+      // Return only allowed routes for restricted roles
+      return allRoutes.admin.filter(
+        (route) =>
+          route.name !== "admin-tools.companies.index" &&
+          route.name !== "admin-tools.administrators.index" &&
+          route.name !== "categoriesAndItems.index" &&
+          route.name !== "admin-tools.branch-data.sites.index" &&
+          route.name !== "admin-tools.branch-data.users.index" &&
+          route.name !== "admin-tools.branch-data.suppliers.index"
+      );
+    }
+    // If not restricted, return all admin routes
+    return allRoutes.admin;
+  };
+
+  console.log("hello", restrictedRoles);
 
   return (
     <Routes>
@@ -33,7 +60,7 @@ const Router = (props: RouteProps) => {
 
       {/* Admin routes with AdminLayout */}
       <Route>
-        {allRoutes.admin.map((route, index) => (
+        {getFilteredAdminRoutes().map((route, index) => (
           <Route
             key={"admin-" + index}
             path={route.path}
