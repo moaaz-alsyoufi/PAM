57aa536c8a27e8f98cc85d294828b2d370a5ac93
Moaaz <moaaz.syf@gmail.com>
2025-01-20 14:30:07 +0200

    show request details on click on notification

--- src/components/layout/admin/components/NotificationButton.tsx
+++ src/components/layout/admin/components/NotificationButton.tsx
@@ -8,16 +8,17 @@ import {
   Dropdown,
   DropdownMenu,
   DropdownToggle,
+  useDialog,
 } from "@/components/daisyui";
 
 import Icon from "@/components/Icon";
 import { Loader } from "@/components/Loader";
 import apiRequest from "@/services/api/api";
 import { useAuthContext } from "@/states/auth";
+import useRequests from "@/pages/admin/dashboard/operations/requests/use-requests";
+import DialogComponent from "@/components/Table/Components/Dialog";
 
 // TODO::
-// 1- if the user click on the notification
-// we should open the request details page to show the user the requested items
 
 // 2- then add approve, reject buttons below it
 
@@ -31,7 +32,11 @@ const NotificationButton = () => {
   const [notifications, setNotifications] = useState<any[]>([]);
   const [loading, setLoading] = useState<boolean>(false);
   const [open, setOpen] = useState(false);
+  const [data, setData] = useState<any[]>([]);
   const dropdownRef = useRef<HTMLDivElement>(null);
+
+  const { getRequestDetails, previewColumns } = useRequests();
+  const { dialogRef, handleShow, handleHide } = useDialog();
 
   const { authState } = useAuthContext();
   const siteId = authState.user?.siteid || 0;
@@ -62,6 +67,18 @@ const NotificationButton = () => {
     await getNotifications();
   };
 
+  const handlePreviewNotification = async (materialId: number) => {
+    console.log(materialId);
+
+    try {
+      const details = await getRequestDetails(materialId);
+      setData(details.details);
+      handleShow();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   useEffect(() => {
     if (siteId) {
       getNotifications();
@@ -85,75 +102,92 @@ const NotificationButton = () => {
   }, []);
 
   return (
-    <div ref={dropdownRef}>
-      <Dropdown vertical={"bottom"} end open={open}>
-        <DropdownToggle
-          className="btn btn-circle btn-ghost btn-sm indicator"
-          button={false}
-          onClick={handleOpenMenu}
-        >
-          {notifications.length > 0 && (
-            <span className="indicator-item bg-warning w-1.5 h-1.5 rounded-full mt-1.5 mr-2.5"></span>
-          )}
-          <Icon icon={bellIcon} fontSize={20} />
-        </DropdownToggle>
-        <DropdownMenu className="card card-compact m-1 w-96 p-3 shadow">
-          <div className="flex items-center justify-between px-2">
-            <p className="text-base font-medium">Notification</p>
-            <Button
-              size={"sm"}
-              shape={"circle"}
-              color={"ghost"}
-              startIcon={<Icon icon={xIcon} fontSize={16} />}
-              onClick={closeMenu}
-            />
-          </div>
-          {loading ? (
-            <Loader />
-          ) : (
-            <>
-              <div className="mt-3 h-96 overflow-y-auto">
-                {notifications.length === 0 ? (
-                  <span>Your notifications are clean</span>
-                ) : (
-                  notifications.map((notification, index) => (
-                    <div
-                      key={index}
-                      className="my-0.5 flex cursor-pointer items-start gap-3 rounded-box p-1.5 transition-all hover:bg-base-content/5 active:scale-[.98]"
-                    >
-                      <div className="grow">
-                        <p className="text-sm">{notification.message}</p>
-                        <p className="text-xs text-base-content/60">Now</p>
+    <>
+      <div ref={dropdownRef}>
+        <Dropdown vertical={"bottom"} end open={open}>
+          <DropdownToggle
+            className="btn btn-circle btn-ghost btn-sm indicator"
+            button={false}
+            onClick={handleOpenMenu}
+          >
+            {notifications.length > 0 && (
+              <span className="indicator-item bg-warning w-1.5 h-1.5 rounded-full mt-1.5 mr-2.5"></span>
+            )}
+            <Icon icon={bellIcon} fontSize={20} />
+          </DropdownToggle>
+          <DropdownMenu className="card card-compact m-1 w-96 p-3 shadow">
+            <div className="flex items-center justify-between px-2">
+              <p className="text-base font-medium">Notification</p>
+              <Button
+                size={"sm"}
+                shape={"circle"}
+                color={"ghost"}
+                startIcon={<Icon icon={xIcon} fontSize={16} />}
+                onClick={closeMenu}
+              />
+            </div>
+            {loading ? (
+              <Loader />
+            ) : (
+              <>
+                <div className="mt-3 h-96 overflow-y-auto">
+                  {notifications.length === 0 ? (
+                    <span>Your notifications are clean</span>
+                  ) : (
+                    notifications.map((notification, index) => (
+                      <div
+                        key={index}
+                        className="my-0.5 flex cursor-pointer items-start gap-3 rounded-box p-1.5 transition-all hover:bg-base-content/5 active:scale-[.98]"
+                        onClick={() =>
+                          handlePreviewNotification(notification.materialId)
+                        }
+                      >
+                        <div className="grow">
+                          <p className="text-sm">{notification.message}</p>
+                          <p className="text-xs text-base-content/60">Now</p>
+                        </div>
+                        <hr />
                       </div>
-                      <hr />
-                    </div>
-                  ))
-                )}
-              </div>
-              <hr className="-mx-2 mt-2 border-base-content/10" />
-              <div className="flex items-center justify-between pt-2">
-                <Button
-                  size={"sm"}
-                  color={"ghost"}
-                  className="text-primary hover:bg-primary/10"
-                  disabled={notifications.length === 0}
-                >
-                  View All
-                </Button>
-                <Button
-                  size={"sm"}
-                  color={"ghost"}
-                  className="text-base-content/80 hover:bg-base-content/10"
-                  disabled={notifications.length === 0}
-                >
-                  Mark as read
-                </Button>
-              </div>
-            </>
-          )}
-        </DropdownMenu>
-      </Dropdown>
-    </div>
+                    ))
+                  )}
+                </div>
+                <hr className="-mx-2 mt-2 border-base-content/10" />
+                <div className="flex items-center justify-between pt-2">
+                  <Button
+                    size={"sm"}
+                    color={"ghost"}
+                    className="text-primary hover:bg-primary/10"
+                    disabled={notifications.length === 0}
+                  >
+                    View All
+                  </Button>
+                  <Button
+                    size={"sm"}
+                    color={"ghost"}
+                    className="text-base-content/80 hover:bg-base-content/10"
+                    disabled={notifications.length === 0}
+                  >
+                    Mark as read
+                  </Button>
+                </div>
+              </>
+            )}
+          </DropdownMenu>
+        </Dropdown>
+      </div>
+
+      <DialogComponent
+        handleHide={handleHide}
+        dialogRef={dialogRef}
+        dialogType={"Preview"}
+        title={"Request Details"}
+        previewColumns={previewColumns}
+        data={data}
+        current={null}
+        onSuccess={() => {}}
+        inputFields={[]}
+      />
+    </>
   );
 };
 
