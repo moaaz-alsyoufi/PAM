c6669e916a178604ef6fc12cc12d1ff1d3cd09c1
Moaaz <moaaz.syf@gmail.com>
2025-01-30 08:57:14 +0200

    return to root page (dashboard) from not found page

--- src/pages/not-found.tsx
+++ src/pages/not-found.tsx
@@ -3,21 +3,20 @@ import error404Image from "@/assets/images/landscape/error-404.svg";
 import { Link } from "react-router-dom";
 
 import PageMetaData from "@/components/PageMetaData";
-import routes from "@/services/routes";
 
 const NotFoundPage = () => {
-    return (
-        <>
-            <PageMetaData title={"Not Found - 404"} />
+  return (
+    <>
+      <PageMetaData title={"Not Found - 404"} />
 
-            <div className="flex h-screen w-screen flex-col items-center justify-center">
-                <img src={error404Image} alt="error" className="max-h-[400px]" />
-                <Link to={routes.landing} className="btn btn-primary mt-5">
-                    Go to Home
-                </Link>
-            </div>
-        </>
-    );
+      <div className="flex h-screen w-screen flex-col items-center justify-center">
+        <img src={error404Image} alt="error" className="max-h-[400px]" />
+        <Link to="/" className="btn btn-primary mt-5">
+          Go to Home
+        </Link>
+      </div>
+    </>
+  );
 };
 
 export default NotFoundPage;
